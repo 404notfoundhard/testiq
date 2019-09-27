52d8ee8238c5f2f0d7d3c4985ada5a2581add031
Stanislav Linnik <404notfoundhard@gmain.com>
2019-09-27 21:31:02 +0700

    commit in two branch

--- test.py
+++ test.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
+import requests
 
 def asdasdasd():
     print('hello fucking world!')
 
 def asdasdasd():
     print('hello fucking world!')
+
+def make_req():
+    request.get('http://example.com')
 
 if __name__ == "__main__":
     asdasdasd()
